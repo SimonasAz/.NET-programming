b6b2fae546c4a5a1b6bcd6c6353e7cc1dc9649f3
Simonas <simonasazguridis@gmail.com>
2025-06-02 23:22:24 +0300

    Number guessing game with C# and ability to play again

---
+++ Week1_C#basics/GuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program{
+    static void Main()
+    {
+        string playAgain;
+        do
+        {
+            PlayGame();
+
+            Console.WriteLine("Do you want to play again? (y/n): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "y");
+        Console.WriteLine("Thanks for playing");
+    }
+
+    static void PlayGame(){
+        Console.WriteLine("Choose a number between 1-100");
+
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        int input = 0;
+        int guesses = 0;
+
+        while (input != number)
+        {
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Try another number: ");
+            if (input < number) Console.WriteLine("Too low");
+            else if (input > number) Console.WriteLine("Too high");
+            else Console.WriteLine("Congrats you've guessed the number");
+            guesses++;
+
+        }
+        Console.WriteLine($"Congrats it took you {guesses} tries");
+
+    }
+}
+
+
